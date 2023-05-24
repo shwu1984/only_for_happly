6b29f46cfc4f868e85e6f7d9820404707b26c0ab
shwu1984 <shwu1984@163.com>
2023-05-24 08:31:23 +0800

    tyyun.py

--- tyyun.py
+++ tyyun.py
@@ -27,7 +27,7 @@ import requests
 import random
 import os
 
-# 变量 ty_username（15605297035）,ty_password（Sshwu@1984*）
+# 变量 ty_username（）,ty_password（）
 ty_username = os.getenv("ty_username").split('&')
 ty_password = os.getenv("ty_password").split('&')
 
